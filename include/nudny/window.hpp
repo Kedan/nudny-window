40f84d4798a2689d2e3eb0f455506b8a41868c35
Piotr Dabrowski <piotrdabrowski@nudne.studio>
2026-06-18 18:11:37 +0200

    [CODE] Window abstract class added

--- include/nudny/window.hpp
+++ include/nudny/window.hpp
@@ -1,1 +1,20 @@
 # pragma once
+
+namespace nd::wnd {
+	class Window {
+		public:
+			virtual unsigned int[2] setSize(int, int) = 0;
+			virtual unsigned int[2] getSize() = 0;
+			virtual unsigned int getWidth() = 0;
+			virtual unsigned int getHeight() = 0;
+
+			virtual bool setFullscreen(bool) = 0;
+			virtual bool getFullscreen() = 0;
+
+			virtual std::string setTitle(std::string) = 0;
+			virtual std::string getString() = 0;
+
+			virtual bool run() = 0;
+
+	}; // class window
+}; //namespace
---
+++ include/nudny/window/sfml.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <nudny/window.hpp>
+
+namespace nd::wnd {
+	class WindowSFML : public Window {
+	}; // class WindowSFML
+}; // namespace nd::wnd
