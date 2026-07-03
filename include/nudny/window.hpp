8ad90b2b3b5c8dfd33016ff204e8b103e0933159
Piotr Dabrowski <piotrdabrowski@nudne.studio>
2026-07-03 17:02:46 +0200

    [CODE] Some changes in code

--- include/nudny/window.hpp
+++ include/nudny/window.hpp
@@ -3,7 +3,7 @@
 namespace nd::wnd {
 	class Window {
 		public:
-			virtual unsigned int[2] setSize(int, int) = 0;
+			virtual unsigned int[2] setSizeNd(int, int) = 0;
 			virtual unsigned int[2] getSize() = 0;
 			virtual unsigned int getWidth() = 0;
 			virtual unsigned int getHeight() = 0;
@@ -11,10 +11,29 @@ namespace nd::wnd {
 			virtual bool setFullscreen(bool) = 0;
 			virtual bool getFullscreen() = 0;
 
-			virtual std::string setTitle(std::string) = 0;
+			virtual std::string setTitle(const std::string&) = 0;
 			virtual std::string getString() = 0;
 
-			virtual bool run() = 0;
+			virtual bool run() {
+				init();
+				return loop();
+			}
+		protected:
+			virtual bool isRunning() = 0;
+			virtual void init();
+			virtual bool loop() {
+				while(isRunning()) {
+					handleEvents();
+					clear();
+					draw();
+					update();
+				}
+				return isRunning();
+			};
+			virtual void handleEvents() = 0;
+			virtual void clear() = 0;
+			virtual void draw() = 0;
+			virtual void update() = 0;
 
 	}; // class window
 }; //namespace
--- include/nudny/window/sfml.hpp
+++ include/nudny/window/sfml.hpp
@@ -1,8 +1,60 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include <nudny/window.hpp>
 
 namespace nd::wnd {
 	class WindowSFML : public Window {
+		public:
+			sf::RenderWindow wnd;
+			
+			unsigned int[2] setSize(unsigned int t_width, unsigned int t_height) {
+				wnd.setSize(sf::Vector2u(t_width,t_height));
+				return getSize();
+			}
+			unsigned int[2] getSize() {
+				return unsigned int[2]{wnd.getSize().x, wnd.getSize().y};
+			}
+			unsigned int getWidth() {
+				return wnd.getSize().x;
+			}
+			unsigned int getHeight() {
+				return wnd.getSize().y;
+			}
+			bool setFullscreen(bool t_fullscreen) {
+				state = t_fullscreen==true ? sf::Fullscreen : sf::Windowed;
+				return getFullscreen();
+			}
+			bool getFullscreen() 
+				return state == sf::Fullscreen ? true : false;{
+			}
+			std::string setTitle(const std::string& t_title) {
+				wnd->setTitle(t_title);
+				return getTitle();
+			}
+			std::string getTitle() {
+				return wnd.getTitle();
+			}	
+		protected:
+			bool isRunning() {
+				return wnd.isOpen();
+			}
+			void handleEvents() {
+				while(const std::optional event = wnd.pollEvent()) {
+					if(event->is<sf::Event::Closed>) {
+						window.close();
+					}
+				}
+			}
+			void clear() {
+				wnd.clear();
+			}
+			void draw() {
+				// draw scene
+				// wnd.draw( object )
+				wnd.display();
+			}
+			:w
+			sf::State state;
 	}; // class WindowSFML
-}; // namespace nd::wnd
+d}; // namespace nd::wnd
